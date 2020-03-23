f031e7a438dad064294d5890308bf63b002b3add
Oscar7-web <ozinc24@hotmail.com>
2020-03-23 17:05:10 +1300

    First Commit

--- app/src/main/java/com/example/bellyful_app/MainActivity.java
+++ app/src/main/java/com/example/bellyful_app/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.bellyful_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_MESSAGE = "com.example.bellyful_app.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the Intent that started this activity and extract the string
+
+    }
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
