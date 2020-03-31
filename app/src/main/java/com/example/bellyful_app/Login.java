cf13285e4150d7c51b8e40d20e5d5e35e65575c2
Oscar7-web <ozinc24@hotmail.com>
2020-04-01 09:59:10 +1300

    commit2

---
+++ app/src/main/java/com/example/bellyful_app/Login.java
@@ -0,0 +1,73 @@
+package com.example.bellyful_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    private Button login;
+    private TextView registerHere;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private ProgressBar loginProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        login = findViewById(R.id.btnLogin);
+        editTextUsername = findViewById(R.id.editTextUserEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        loginProgressBar = findViewById(R.id.loginProgressBar);
+
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Hide the virtual keyboard
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(view.getApplicationWindowToken(),0);
+
+                //Show progress bar
+                loginProgressBar.setVisibility(View.VISIBLE);
+
+                String usernameText = editTextUsername.getText().toString();
+                String passwordText = editTextPassword.getText().toString();
+
+                loginProgressBar.setVisibility(View.GONE); //
+
+                Intent startMainIntent = new Intent(Login.this, MainActivity.class);
+                Login.this.startActivity(startMainIntent);
+
+                /*boolean isValid = validate(usernameText, passwordText);
+                if(isValid){
+                    signIn(usernameText, passwordText);
+                }else{
+                    //Some error occurred
+                    loginProgressBar.setVisibility(View.GONE);
+                }*/
+            }
+        });
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/bellyful_app/MainActivity.java
+++ app/src/main/java/com/example/bellyful_app/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.bellyful_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-
-
     public static final String EXTRA_MESSAGE = "com.example.bellyful_app.MESSAGE";
+    /*BottomNavigationView bottomNavigationView; // Bottom navigation bar
+    private ArrayList<JobData> newJobList = new ArrayList<>(); // New jobs to pass to the NewJobFragment
+    private ArrayList<FreezerModel> freezerList = new ArrayList<>(); // Freezer info to pass to the FreezerFragment
+    protected ArrayList <AcceptedJobModel> selectedJobList = new ArrayList<>(); // Jobs for the CurrentJobFragment
+    */
+    //private FirebaseAuth mAuth = FirebaseAuth.getInstance(); // Initialize an instance of Firebase Auth
+    //private FirebaseDatabase database = FirebaseDatabase.getInstance(); // Initialize an instance of the database
+
+    Toolbar mToolbar;
+    Fragment ft;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Intent startLoginIntent = new Intent(MainActivity.this, Login.class);
+        MainActivity.this.startActivity(startLoginIntent);
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
 
     }
-    public void sendMessage(View view) {
+    /*public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 }
